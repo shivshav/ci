bcc0b86c9fe76044e16196b94b7b0c471387f1db
cgudea <pulala@gmail.com>
2016-01-28 12:46:12 -0800

    run tests headless and force test to run in top level ci project resolves #7 resolves #2

--- test/integration/test_redmine_backup_restore.py
+++ test/integration/test_redmine_backup_restore.py
@@ -1,9 +1,21 @@
 import subprocess, os, re
 from selenium import webdriver
+from pyvirtualdisplay import Display
 
+def wrong_dir_error():
+    print "ERROR\tTest ran from wrong directory!"
+    print "\tRun test from top level directory, outside of test folder"
+    exit(1)
+
+def check_path_test_dir(current_dir):
+    for x in ["test","integration"]:
+        if x in current_dir:
+            wrong_dir_error()
 
 def log_into_ci():
-    global DRIVER
+    global DRIVER, DISPLAY
+    DISPLAY = Display(visible=0, size=(800,600))
+    DISPLAY.start()
     DRIVER = webdriver.Firefox() # initilize selenium
     DRIVER.get("http://admin:passwd@localhost/redmine")
 
@@ -15,14 +27,14 @@ def log_into_redmine():
     element.send_keys("admin")
     DRIVER.find_element_by_name("login").click()
 
-def create_project():
+def create_redmine_project():
     DRIVER.get("http://localhost/redmine/projects")
     DRIVER.get("http://localhost/redmine/projects/new")
     DRIVER.find_element_by_id("project_name").send_keys("Test Project")
     DRIVER.find_element_by_name("commit").click()
 
 
-def create_issue():
+def create_redmine_issue():
     DRIVER.get("http://localhost/redmine/projects/test-project/issues/new")
     DRIVER.find_element_by_id("issue_subject").send_keys("Test Issue of Type Bug")
     DRIVER.find_element_by_name("commit").click()
@@ -31,22 +43,23 @@ def create_issue():
 
 def setup_function(function):
     current_dir = os.getcwd() # get current directory
-    test_dir = os.path.abspath(os.path.join(current_dir, os.pardir)) # get parent's  absolute path
+    check_path_test_dir(current_dir) # ensure we aren't in the test directory
+    test_dir = os.path.abspath(os.path.join(current_dir, "test/integration")) # get test directory path
     global PROJECT_DIR
-    PROJECT_DIR = os.path.abspath(os.path.join(test_dir, os.pardir)) # get parent's parent absolute path
+    PROJECT_DIR = current_dir 
     recreate_path = os.path.join(PROJECT_DIR, 'recreate.sh') # restart ci environment
     subprocess.call([recreate_path]) # call run script (how do I do verbose)?
-    # Popen([recreate_path]).wait()
 
     log_into_ci()
     log_into_redmine()
-    create_project()
-    create_issue()
+    create_redmine_project()
+    create_redmine_issue()
 
 
 def teardown_function(function):
     #TODO: Understand pytest scope, setup, and tear down along with execution sequence
     DRIVER.close()
+    DISPLAY.stop()
 
 def test_redmine_backup_and_restore():
     # Run backup script
